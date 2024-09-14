b445e3c304cb33e060bb505428212099063cf197
Adlok53 <01mikinini@gmail.com>
2024-09-14 11:52:53 +0300

    Документация

--- ex1.py
+++ ex1.py
@@ -1,15 +1,21 @@
+# Приветсвие
 print("Добро пожаловать на поисковик")
 
+# Пример данных
 list = [
     {"text": "test", "created_date": "1", "rubrics": "example"}
 ]
 
+# Загрузка данными
 text = input("Введите заголовок ")
 created_date = input("Введите дату ")
 rubrics = input("Введите рубрику ")
 
+# Сохранение данных
 add = {"text": text, "created_date": created_date, "rubrics": rubrics}
 
+# Добавление введеных данных
 list.append(add)
 
+# Вывод результата
 print(list)
